0eb975e00b6c7132a6c324bf1a3fef495ff306f4
vietpq <20522153@gm.uit.edu.vn>
2026-03-06 03:56:02 +0000

    refactor confirmModal

--- src/react-app/features/modal/modals/ConfirmModal.tsx
+++ src/react-app/features/modal/modals/ConfirmModal.tsx
@@ -1,39 +1,47 @@
 /* FILE: src/react-app/features/modal/modals/ConfirmModal.tsx */
-import { Dialog, DialogTitle, DialogActions, Button, DialogContent, Typography } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { closeModal } from "../modalSlice";
-import { removeNote } from "../../notes/notesSlice";
-import { AppDispatch } from "../../../app/store";
+import { Dialog, DialogTitle, DialogActions, Button, DialogContent, Typography } from "@mui/material"
+import { useDispatch } from "react-redux"
+import { closeModal } from "../modalSlice"
+import { removeNote } from "../../notes/notesSlice"
+import { AppDispatch } from "../../../app/store"
 
-type Props = {
-  title?: string;
-  message: string;
-  itemId: string;
-  actionType: "DELETE_NOTE";
-};
+interface ConfirmModalProps {
+  title?: string
+  message?: string
+  itemId: string
+  actionType: "DELETE_NOTE"
+}
 
-export default function ConfirmModal({ title, message, itemId, actionType }: Props) {
-  const dispatch = useDispatch<AppDispatch>();
+export default function ConfirmModal({ 
+  title = "Confirm Action", 
+  message = "Are you sure you want to proceed?", 
+  itemId, 
+  actionType 
+}: ConfirmModalProps) {
+  const dispatch = useDispatch<AppDispatch>()
 
-  const handleClose = () => dispatch(closeModal());
+  const handleClose = () => dispatch(closeModal())
 
-  const handleConfirm = () => {
+  const handleConfirm = async () => {
     if (actionType === "DELETE_NOTE") {
-        dispatch(removeNote(itemId)); // ItemID is the Note ID
+      // Logic is central: the thunk handles API, Notifications, and State
+      await dispatch(removeNote(itemId))
     }
-    dispatch(closeModal());
-    };
+    dispatch(closeModal())
+  }
 
   return (
     <Dialog open onClose={handleClose} fullWidth maxWidth="xs">
-      <DialogTitle>{title || "Confirm"}</DialogTitle>
+      <DialogTitle>{title}</DialogTitle>
       <DialogContent>
         <Typography>{message}</Typography>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
-        <Button color="error" variant="contained" onClick={handleConfirm}>Confirm</Button>
+        <Button color="error" variant="contained" onClick={handleConfirm} disableElevation>
+          Confirm
+        </Button>
       </DialogActions>
     </Dialog>
-  );
+  )
 }
